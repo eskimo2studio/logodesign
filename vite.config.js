e3e0bfcec73d644055faee37380cc45c708146f6
Eskimo <103266397+eskimo2studio@users.noreply.github.com>
2026-08-01 22:44:35 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/logodesign/', // สำคัญมาก ต้องมีเครื่องหมาย / ครอบหัวและท้าย
+  base: '/logodesign/', // ต้องใส่ชื่อ repository ครอบด้วย / ทั้งหน้าและหลัง
 })
