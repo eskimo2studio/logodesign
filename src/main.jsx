3a8e1cea3894e39c2dfef467a680b5a03719a16d
Eskimo <103266397+eskimo2studio@users.noreply.github.com>
2026-08-01 22:43:10 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import Portfolio from './Portfolio.jsx'
@@ -12,7 +12,7 @@ import BlogDetail from './BlogDetail.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
@@ -22,6 +22,6 @@ createRoot(document.getElementById('root')).render(
         <Route path="/blog/:id" element={<BlogDetail />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
