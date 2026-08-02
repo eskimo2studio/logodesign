f60c546609d451ae8a19c0ff32097b4ae7c5930e
Prachya <pockprachya@gmail.com>
2026-08-02 09:14:42 +0700

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/logodesign/',
   plugins: [react()],
-  base: '/logodesign/', // ต้องใส่ชื่อ repository ครอบด้วย / ทั้งหน้าและหลัง
 })
